713256c7d8fcb08da6ab56ae6181654617d069a0
jiawen.ling <jling002@e.ntu.edu.sg>
2023-03-08 22:34:19 +0800

    WIP JWT

---
+++ demo/react-app/src/AuthUtil.js
@@ -0,0 +1,35 @@
+const login = (username, password) => {
+    return axios
+      .post(API_URL + "signin", {
+        username,
+        password,
+      })
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+  
+        return response.data;
+      });
+  };
+  
+  const isLoggedIn = () => localStorage.getItem("user")!==null
+
+  const logout = () => {
+    localStorage.removeItem("user");
+  };
+  
+  const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("user"));
+  };
+  
+  const AuthUtil = {
+    register,
+    login,
+    isLoggedIn,
+    logout,
+    getCurrentUser,
+  };
+  
+  export default AuthUtil;
+  
--- demo/src/main/java/com/example/demo/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -5,31 +5,18 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.demo.Service.UserDetailServiceImpl;
 
@@ -44,6 +31,9 @@ public class WebSecurityConfig  {
 
     @Autowired
     UserDetailServiceImpl userDetailServiceImpl;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -72,18 +62,18 @@ public class WebSecurityConfig  {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable()
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/login", "/api/login").permitAll()
-                        .requestMatchers("/api/manager").hasRole("MANAGER")
-                        .requestMatchers("/css/**","/js/**","/images/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin().loginPage("/login").permitAll().loginProcessingUrl("/api/login")
-                .and()
-                .headers(headers -> headers.frameOptions().disable())           
-                        .userDetailsService(userDetailServiceImpl);
+            .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .authorizeHttpRequests(auth -> auth
+            .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+            .requestMatchers("/").permitAll()
+            .requestMatchers("/login", "/api/login").permitAll()
+            .requestMatchers("/api/manager").hasRole("MANAGER")
+            .requestMatchers("/css/**","/js/**","/images/**").permitAll()
+            .anyRequest().authenticated()
+            )
+            .headers(headers -> headers.frameOptions().disable());           
+                        
                 
         http
                 .headers().frameOptions().disable();
